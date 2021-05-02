c8fcc71583c36ff32e01ef61b4503834a425e2e4
Saurav Kumar <maxsaurav267@gmail.com>
2021-05-02 10:20:02 +0530

    added watch later page

---
+++ src/components/WatchLaterPage.jsx
@@ -0,0 +1,10 @@
+import { Sidebar } from "./index";
+import { WatchLater } from "./index";
+export function WatchLaterPage(){
+    return(
+        <div className="grid">
+            <Sidebar/>
+            <WatchLater/>
+        </div>
+    )
+} 
