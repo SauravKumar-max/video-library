390f967a557b4aca4e1f971f147d0f225df67143
Saurav Kumar <maxsaurav267@gmail.com>
2021-05-02 09:27:39 +0530

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { Navbar, Home, VideoPlayer, History, WatchLaterPage, LikedPage, PlaylistPage, PlaylistModal, PlaylistItems} from "./components/index";
+import { Routes, Route } from "react-router-dom";
+import { usePlaylist } from './context/playlist-context';
+
 
 function App() {
+  const { statePlaylist } = usePlaylist();
+
   return (
     <div className="App">
-     
+      
+      { statePlaylist.toggleModal !== null && <PlaylistModal/> }
+      <Navbar/>
+      <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/video/:id" element={ <VideoPlayer/> } />
+          <Route path="/watch-later" element={ <WatchLaterPage/>} />
+          <Route path="/liked-videos" element={ <LikedPage/> } />
+          <Route path="/playlists" element={ <PlaylistPage/> } />
+          <Route path="/playlists/:id" element={ <PlaylistItems/> } />
+          <Route path="/history" element={ <History/> } />
+      </Routes>
+   
     </div>
   );
 }
