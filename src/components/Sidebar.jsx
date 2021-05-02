9e9b641277744067fd9409d8e24357d03a49b7d7
Saurav Kumar <maxsaurav267@gmail.com>
2021-05-02 09:31:08 +0530

    created sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router-dom";
+
+export function Sidebar(){
+    return(
+        <div className="sidebar-container">
+            <ul className="sidebar-lists">
+                
+                <li className="sidebar-item">
+                    <NavLink end to="/" activeStyle={{color: "#3d41d7"}}>
+                        <i className="fas fa-home"></i>
+                        Home
+                    </NavLink>
+                </li>
+
+                <li className="sidebar-item">
+                    <NavLink to="/playlists" activeStyle={{color: "#3d41d7"}}>
+                        <i className="fas fa-list"></i>
+                        Playlist
+                    </NavLink>
+                </li>
+
+                <li className="sidebar-item">
+                    <NavLink to="/watch-later" activeStyle={{color: "#3d41d7"}}>
+                        <i className="fas fa-clock"></i>
+                        Watch Later
+                    </NavLink> 
+                </li>
+
+                <li className="sidebar-item">
+                    <NavLink to="/liked-videos" activeStyle={{color: "#3d41d7"}}>
+                        <i className="fas fa-thumbs-up"></i>
+                        Liked Videos
+                    </NavLink>
+                </li>
+
+                <li className="sidebar-item">
+                    <NavLink to="/history" activeStyle={{color: "#3d41d7"}}>
+                        <i className="fas fa-history"></i>
+                        History
+                    </NavLink>
+                </li>
+
+            </ul>            
+        </div>
+    )
+}
+
