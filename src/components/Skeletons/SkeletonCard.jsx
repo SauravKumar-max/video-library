3b0e6adda13f0e317f748aea9749fc5243841dc0
Saurav Kumar <maxsaurav237@gmail.com>
2021-08-06 20:45:11 +0530

    created skeleton for video cards

---
+++ src/components/Skeletons/SkeletonCard.jsx
@@ -0,0 +1,11 @@
+
+export function SkeletonCard(){
+    return(
+        <div className="skeleton-container">
+            <div className="skt-thumbnail"></div>
+            <div className="skt-channel-img"></div>
+            <div className="skt-title"></div>
+            <div className="skt-channelname"></div>
+        </div>
+    )
+}
