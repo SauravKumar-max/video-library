b2013161c98cfa08117965d404ea90f0c950ddcb
Saurav Kumar <maxsaurav237@gmail.com>
2021-08-06 21:39:57 +0530

    added skeleton body for video card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,22 @@
-import { Sidebar, VideoCard } from "../components/index";
+import { Sidebar, SkeletonCard, VideoCard } from "../components/index";
 import { useVideo } from "../context/video-context";
 
 export function Home(){
-    const { videoData } = useVideo();
+    const { videoData, loader } = useVideo();
+    const skeleton =  [...Array(12)];
 
     return(
         <div className="grid">
             <Sidebar/>
             <div className="videolist-container">
-                {videoData?.map(item => {
-                    return(
-                        <VideoCard key={item._id} item={item}/>
-                    )
-                })}
+                {loader && skeleton.map((element, i) => <SkeletonCard key={i}/>)}
+                {
+                    videoData?.map(item => {
+                        return(
+                            <VideoCard key={item._id} item={item}/>
+                        )
+                    })
+                }
             </div>
         </div>
     )
