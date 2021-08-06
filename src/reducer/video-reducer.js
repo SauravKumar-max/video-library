0d79b26921ec3542a4f8bfe35c24396367a09631
Saurav Kumar <maxsaurav237@gmail.com>
2021-08-06 21:25:20 +0530

    ref: video reducer

--- src/reducer/video-reducer.js
+++ src/reducer/video-reducer.js
@@ -1,26 +1,12 @@
 
 export const videoReducer = (state, action) => {    
     switch (action.type) {
-        case "PLAY_VIDEO":
-            return { ...state, currentVideo: state.data.find(item => item.id === action.payload)};
+        case "TOGGLE_PROFILE_CARD":
+            return { ...state, toggleProfile: !state.toggleProfile };
 
-        case "ADD_TO_HISTORY":
-            return {...state, history: [ ...state.history.filter(item => item.id !== action.payload.id), action.payload ]};
-
-        case "ADD_TO_LIKE": 
-            return {...state, liked: [...state.liked, action.payload] };
-
-        case "REMOVE_FROM_LIKE": 
-            return {...state, liked: [...state.liked.filter(item => item.id !== action.payload)] };
-        
-        case "ADD_TO_WATCHLATER": 
-            return {...state, watchLater: [...state.watchLater, action.payload]};
-
-        case "REMOVE_FROM_WATCHLATER":
-            return { ...state, watchLater: [ ...state.watchLater.filter(item => item.id !== action.payload)]};
-        
-        case "ADD_NOTES":
-            return { ...state, data: [...state.data.map(item => item.id === state.currentVideo.id ? {...item, allNotes: [...item.allNotes, action.payload] } : item )]}
+        case "TOGGLE_LOGIN_MODAL":
+            return { ...state, toggleLoginModal: !state.toggleLoginModal };
+       
         default:
             break;
     }
