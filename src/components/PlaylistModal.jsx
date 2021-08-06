f6792c3e780f0f969c64d403316af422e3a8b7ec
Saurav Kumar <maxsaurav237@gmail.com>
2021-08-06 20:49:17 +0530

    feat: add and remove playlist api requests

--- src/components/PlaylistModal.jsx
+++ src/components/PlaylistModal.jsx
@@ -1,9 +1,33 @@
-import { usePlaylist } from "../context/playlist-context";
+import axios from "axios";
+import { useState } from "react";
+import { useData } from "../context/userdata-context";
 import { CreatePlaylist } from "./index";
 
 export function PlaylistModal(){
-    const { statePlaylist, dispatchPlaylist } = usePlaylist();
-    const { playlist, toggleModal } = statePlaylist;
+    const [ toggle, setToggle ] = useState(false);
+    const { stateData, dispatchData } = useData();
+    const { playlist, playlistModal } = stateData;
+    const { _id: videoId } = playlistModal.video;
+
+    async function addToPlaylist(item){
+        try {
+            const api = 'https://Video-Library-Backend.sauravkumar007.repl.co/userdata/playlist';
+            axios.post(api, { name: item.name, _id: videoId });
+            dispatchData({ type: "ADD_TO_PLAYLIST", payload: {playlistId: item._id, video: playlistModal.video} });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function removeFromPlaylist(playlistId){
+        dispatchData({type: "REMOVE_FROM_PLAYLIST", payload: { playlistId, videoId }});
+        try {
+            const api = 'https://Video-Library-Backend.sauravkumar007.repl.co/userdata/playlist/remove';
+            await axios.post(api, { playlistId, videoId });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return(
         <div className="playlist-modal">
@@ -11,23 +35,36 @@ export function PlaylistModal(){
                 <div className="playlist-heading">
                     <p>Add To.. </p>
                     <i onClick={() => {
-                        dispatchPlaylist({type: "HIDE_MODAL"});
-                        dispatchPlaylist({type: "HIDE_CREATE"})
+                        dispatchData({type: "TOGGLE_MODAL", payload: null});
                     }} 
                     className="fas fa-times">
                     </i>
                 </div>
 
                 <div className="playlist-items">
+                    { 
+                        playlist.length === 0 && 
+                            <p style={{
+                                margin: "auto", 
+                                fontSize: "0.9rem", 
+                                fontWeight: "bold", 
+                                color: "#3d41d7"
+                            }}> 
+                                Create your first Playlist. 
+                            </p> 
+                    }
+
                     {
-                        Object.keys(playlist).map(key => {
+                        playlist.map(item => {
+                            const findVideo = item.list.find(item => item._id === playlistModal.video._id);
                             return(
-                                <label key={key}>
+                                <label key={item._id}>
                                     <input 
-                                    checked={playlist[key].find(item => item.id === toggleModal.id) ? true : false}
-                                    onChange={ () => playlist[key].find(item => item.id === toggleModal.id) ? dispatchPlaylist({type: "REMOVE_FROM_PLAYLIST", payload: key }) : dispatchPlaylist({type: "ADD_TO_PLAYLIST", payload: key }) }
-                                    type="checkbox"/>
-                                    {key}
+                                        checked={ findVideo ? true : false}
+                                        onChange={ () => findVideo ? removeFromPlaylist(item._id) : addToPlaylist(item) }
+                                        type="checkbox"
+                                    />
+                                    {item.name}
                                 </label>
                             )
                         })
@@ -35,11 +72,11 @@ export function PlaylistModal(){
                 </div>
 
                 {
-                    statePlaylist.createPlaylist === "show" ?
+                    toggle ?
                     <CreatePlaylist/>
                     :    
                     <button className="create-btn"
-                        onClick={() => dispatchPlaylist({ type: "SHOW_CREATE"})} 
+                        onClick={() => setToggle(true)} 
                     >
                         + Create new playlist
                     </button>
