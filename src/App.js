86d8af844f0f69036311e697956acfe47b527245
Saurav Kumar <maxsaurav237@gmail.com>
2021-08-06 21:37:28 +0530

    added private route for pages and login modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import './App.css';
-import { Navbar, Home, VideoPlayer, History, WatchLaterPage, LikedPage, PlaylistPage, PlaylistModal, PlaylistItems} from "./components/index";
+import { LoginModal, Navbar, PlaylistModal } from "./components/index";
 import { Routes, Route } from "react-router-dom";
-import { usePlaylist } from './context/playlist-context';
+import { Home, VideoPlayer, History, WatchLater, Like, Login, SignUp, Playlist, PlaylistItems, PrivateRoute } from "./pages/index";
+import { useData } from './context/userdata-context';
+import { useVideo } from './context/video-context';
 
 
 function App() {
-  const { statePlaylist } = usePlaylist();
-
+  const { stateData } = useData();
+  const { state } = useVideo();
   return (
     <div className="App">
-      
-      { statePlaylist.toggleModal !== null && <PlaylistModal/> }
+      { stateData.playlistModal.show && <PlaylistModal/> }
+      { state.toggleLoginModal && <LoginModal/> }
       <Navbar/>
       <Routes>
           <Route path="/" element={ <Home/> } />
           <Route path="/video/:id" element={ <VideoPlayer/> } />
-          <Route path="/watch-later" element={ <WatchLaterPage/>} />
-          <Route path="/liked-videos" element={ <LikedPage/> } />
-          <Route path="/playlists" element={ <PlaylistPage/> } />
-          <Route path="/playlists/:id" element={ <PlaylistItems/> } />
-          <Route path="/history" element={ <History/> } />
+          <Route path="/login" element={ <Login/> } />
+          <Route path="/signup" element={ <SignUp/> } />
+          <PrivateRoute path="/watch-later" element={ <WatchLater/>} />
+          <PrivateRoute path="/liked-videos" element={ <Like/> } />
+          <PrivateRoute path="/playlists" element={ <Playlist/> } />
+          <PrivateRoute path="/playlists/:id" element={ <PlaylistItems/> } />
+          <PrivateRoute path="/history" element={ <History/> } />
       </Routes>
    
     </div>
