a0f9fdd26726d88203cbff8254f211b2b8cf4ee0
Saurav Kumar <maxsaurav237@gmail.com>
2021-08-06 20:54:15 +0530

    changed link to navigate

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,36 +1,45 @@
-import { useVideo } from "../context/video-context";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { WatchLaterBtn } from "./index";
-import { usePlaylist } from "../context/playlist-context";
+import { useData } from "../context/userdata-context";
+import { useVideo } from "../context/video-context";
+import { useAuth } from "../context/auth-context";
 
 export function VideoCard({item}){
-    const { dispatchPlaylist } = usePlaylist();
-    const { dispatch } = useVideo();
-    const {id, thumbnail, title, channelImage, channelName, time, duration} = item;
+    const { dispatchData } = useData();
+    const { _id, thumbnail, title, channelImage, channelName, time, duration } = item;
+    const { dispatch } = useVideo()
+    const { login } = useAuth();
+    const navigate = useNavigate();
     
     return(
             <div className="videocard-container">
-                
                 <div className="positioned">
-                    <Link to={`/video/${id}`} onClick={() => {
-                        dispatch({type:"PLAY_VIDEO", payload: id});
-                        dispatch({type: "ADD_TO_HISTORY", payload: item});
-                    }}>
-                        <img className="thumbnail" src={thumbnail} alt={title}/>
-                    </Link>
+                    <div className="img-container">
+                        <img 
+                            className="thumbnail"
+                            onClick={ () => navigate(`/video/${_id}`)} 
+                            src={thumbnail} 
+                            alt={title}
+                        />
+                    </div>
                     <WatchLaterBtn item={item}/>
-                    <div onClick={() => dispatchPlaylist({type:"SHOW_MODAL", payload: item})} className="playlist-btn"><i className="fas fa-list"></i></div>
+                    <div className="playlist-btn"
+                        onClick={() => login ? dispatchData({ type:"TOGGLE_MODAL", payload: item }) : dispatch({type: "TOGGLE_LOGIN_MODAL"})} 
+                    >
+                        <i className="fas fa-list"></i>
+                    </div>
                 </div>
                 <div className="videocard-info">
                     <div className="duration">{duration}</div>
                     <img className="normal-img" src={channelImage} alt="channel"/>
                     <div className="videocard-text">
-                        <Link to={`/video/${id}`} className="title" onClick={() => {
-                            dispatch({type:"PLAY_VIDEO", payload: id});
-                            dispatch({type: "ADD_TO_HISTORY", payload: item});
-                        }}>
+                        <div 
+                            onClick={ () => navigate(`/video/${_id}`)} 
+                            className="title" 
+                            title={title} 
+                        >
                             {title}
-                        </Link>
+                        </div>
                         <p className="channel-name">{channelName}</p>
                         <p className="time">{time}</p>
                     </div>
